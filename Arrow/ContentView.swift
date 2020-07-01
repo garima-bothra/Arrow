3ea0be1de79a02558f59e3d438c81a4901bf94d6
garima94921 <gaarimabothra@gmail.com>
2020-07-02 04:42:19 +0530

    Added: Arrow View and animatableData

--- Arrow/ContentView.swift
+++ Arrow/ContentView.swift
@@ -8,9 +8,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arrow: View {
+
+    var lineWidth: CGFloat
+
+    var animatableData: CGFloat {
+        get { (lineWidth) }
+        set { self.lineWidth = (newValue)}
+    }
+
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 0) {
+            Triangle()
+                .frame(width: (lineWidth * 2.5), height: (lineWidth * 2.5), alignment: .center)
+            Rectangle()
+                .frame(width: (lineWidth), height: (lineWidth * 2.5), alignment: .center)
+        }
+    }
+}
+
+struct ContentView: View {
+
+    @State private var width: CGFloat = 100
+    var body: some View {
+        VStack {
+        Arrow(lineWidth: width)
+            .onTapGesture {
+                withAnimation {
+                self.width -= 10.0
+                }
+            }
+        }
     }
 }
 
